c9d19317a321c0afdadce2520b026ffe3def8050
Alejandro_Fuentes_Gómez <alejandro.fuentes@guadaltech.es>
2021-02-18 13:59:01 +0100

    [IMP] Components Added components in order to learn how components work.

--- src/App.js
+++ src/App.js
@@ -1,52 +1,21 @@
 import './App.css';
 import React, { Component } from 'react';
-/* function Helloworld(props){
-  return (
-    <div id="hello">
-    <h3>{props.subtitle}</h3>
-    {props.text}
-    </div>
-  );
-}
- */
-class Helloworld extends React.Component{
+import './App.css';
+import tasks from './sample/tasks.json';
+import Tasks from './components/Tasks';
+import TaskForm from './components/TaskForm';
 
-  state={
-    show:true
-  }
-  toggleShow = () => {
-    this.setState({show: !this.state.show})
+class App extends Component{
+
+  state = {
+    tasks:tasks
   }
 
   render(){
-    if(this.state.show){
-      return(
-        <div id="hello">
-        <h3>{this.props.subtitle}</h3>
-        {this.props.text}
-        <button onClick={this.toggleShow}>Hide info</button>
-        </div>
-      )
-    }
-    else{
-      return(
-        <div>
-          <h1>There are not elements</h1>
-          <button onClick={()=> this.setState({show:true})}>Show info</button>
-        </div>
-      ) 
-    }
+    return <div>
+    <TaskForm/>
+    <Tasks tasks={this.state.tasks}/>
+    </div>
   }
 }
-
-function App() {
-  return (
-  <div>This is my component: 
-    <Helloworld text="May the force be with you" subtitle="Lorem Impsum"/>
-    <Helloworld text="Game of thrones" subtitle="Caesar"/>
-    <Helloworld text="Harry Potter" subtitle="Mariae"/>
-  </div>
-  );
-}
-
 export default App;
