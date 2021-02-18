75d4b8bb301f07a503a9a6ca66061a57027234c7
Alejandro_Fuentes_Gómez <alejandro.fuentes@guadaltech.es>
2021-02-18 11:15:10 +0100

    [IMP] First functions in React Added some functions in order to familiarize the react environment.

--- src/App.js
+++ src/App.js
@@ -1,24 +1,51 @@
-import logo from './logo.svg';
 import './App.css';
+import React, { Component } from 'react';
+/* function Helloworld(props){
+  return (
+    <div id="hello">
+    <h3>{props.subtitle}</h3>
+    {props.text}
+    </div>
+  );
+}
+ */
+class Helloworld extends React.Component{
+
+  state={
+    show:true
+  }
+  toggleShow = () => {
+    this.setState({show: !this.state.show})
+  }
+
+  render(){
+    if(this.state.show){
+      return(
+        <div id="hello">
+        <h3>{this.props.subtitle}</h3>
+        {this.props.text}
+        <button onClick={this.toggleShow}>Hide info</button>
+        </div>
+      )
+    }
+    else{
+      return(
+        <div>
+          <h1>There are not elements</h1>
+          <button onClick={()=> this.setState({show:true})}>Show info</button>
+        </div>
+      ) 
+    }
+  }
+}
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+  <div>This is my component: 
+    <Helloworld text="May the force be with you" subtitle="Lorem Impsum"/>
+    <Helloworld text="Game of thrones" subtitle="Caesar"/>
+    <Helloworld text="Harry Potter" subtitle="Mariae"/>
+  </div>
   );
 }
 
